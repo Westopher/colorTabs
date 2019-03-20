256ecb990e11a0ab82d4ad00a3a51b2b0090c448
West Kraemer <kraemerwest@gmail.com>
2019-03-20 15:27:26 -0400

    add vc files and connect to each view

---
+++ colorTabs/BlueVC.swift
@@ -0,0 +1,21 @@
+//
+//  BlueVC.swift
+//  colorTabs
+//
+//  Created by West Kraemer on 3/20/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class BlueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
