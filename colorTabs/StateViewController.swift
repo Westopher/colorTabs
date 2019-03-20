444c54abc614c4614bcc407287fb5ac85de44678
West Kraemer <kraemerwest@gmail.com>
2019-03-20 16:26:26 -0400

    add vc's and embed in tab view

--- colorTabs/StateViewController.swift
+++ colorTabs/StateViewController.swift
@@ -12,12 +12,29 @@ class StateViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         print("view did load")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        //if something is going to take a couple of seconds, put it in here. This way the rest of the view can load and your network call can then load after everything else has already appeared.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view did disappear")
+    }
 
 
 }
