ed784005e17388454eb93e74c203e0064b8b07a2
West Kraemer <kraemerwest@gmail.com>
2019-03-20 15:49:20 -0400

    add state VC and connect

---
+++ colorTabs/StateViewController.swift
@@ -0,0 +1,23 @@
+//
+//  StateViewController.swift
+//  colorTabs
+//
+//  Created by West Kraemer on 3/20/19.
+//  Copyright © 2019 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class StateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("view did load")
+    }
+    
+
+   
+
+
+}
